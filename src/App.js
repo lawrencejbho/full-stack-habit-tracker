b563fbfa4b322675e6bc33acb39030e34da7e1b7
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-05-20 17:55:22 -0700

    added in paper and card componenets from mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Card, Paper, Grid } from "@mui/material";
+import { Card, Grid, Button, makeStyles, Paper } from "@mui/material";
+import randomColor from "randomcolor";
 
 function App() {
   //code to get the date, use an array of months, and display the date as a string
   // TODO in the future you'll probably need to initialize this in state
   // TODO will also need a way to store this into an object so that we can store for analytics
+
   let date = new Date();
   const month = [
     "January",
@@ -26,8 +28,21 @@ function App() {
 
   return (
     <div>
-      <h1> Habit Tracker </h1>
-      {todayDateString}
+      {/* First paper will be the toolbar? */}
+      <Paper elevation={10} style={{ background: randomColor() }}>
+        <Grid container>
+          <h1>Habit Tracker</h1>
+        </Grid>
+        <Grid container>
+          <h3>{todayDateString}</h3>
+        </Grid>
+      </Paper>
+      {/* Second paper is the app? */}
+      <Paper elevation={20} style={{ background: randomColor() }}>
+        <Card>first</Card>
+        <Card>second</Card>
+        <Card>third</Card>
+      </Paper>
     </div>
   );
 }
