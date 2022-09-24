784df0649d42ad8e73c56834658b7f69c790e61a
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-09-23 21:38:54 -0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred" });
 });
