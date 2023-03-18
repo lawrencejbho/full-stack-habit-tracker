d4b64ae813ea210b15ceeb85ddf9bb281a58abb6
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-03-18 14:19:16 -0700

    testing netlify

---
+++ frontend/src/pages/Home3.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Header from "../components/landingPage/Header.js";
+import Hero from "../components/landingPage/Hero.js";
+import Features from "../components/landingPage/Features.js";
+import Faq from "../components/landingPage/Faq.js";
+import Contact from "../components/landingPage/Contact.js";
+import Footer from "../components/landingPage/Footer.js";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Faq />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
